371a4bfb4b7d9c107628a968c1adae3eb400f7a0
Keisukemuramatsu <harukasoranoyouni@gmail.com>
2021-08-19 07:17:18 +0000

    [Add]会員情報編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :customers
+  devise_for :customers, controllers: { registrations: 'customer/registrations' }
+  
   devise_for :admins, path: 'admin'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.htmlend
   namespace :admin do
@@ -10,5 +11,7 @@ Rails.application.routes.draw do
   namespace :public, path: "" do
     root to: "homes#top" 
     get '/about' => 'homes#about'
+    resource :customers, only: [:show]
+    
   end
 end
