a560e56dd25d0b381df4cf2b683a7459c641dc08
Keisukemuramatsu <harukasoranoyouni@gmail.com>
2021-08-30 09:25:30 +0000

    [Add]住所選択機能

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -15,12 +15,21 @@ class Public::OrdersController < ApplicationController
         
     end
     def confirm
-     @order = Order.new(order_params)
-     @address = Address.find(params[:order][:address_id])
-     @order.postal_code = @address.postal_code
-     @order.address = @address.address
-     @order.name = @address.name
+    @order = Order.new(order_params)
+    if params[:order][:select_address] == "0"
+    @order.postal_code = current_customer.postal_code
+    @order.address = current_customer.address
+    @order.name = current_customer.last_name + current_customer.first_name
+    elsif params[:order][:select_address] == "1"
+    @address = Address.find(params[:order][:address_id])
+    @order.postal_code = @address.postal_code
+    @order.address = @address.address
+    @order.name = @address.name
+    elsif params[:order][:select_address] == "2"
+    @order.save
+    end
      @cart_items = CartItem.where(customer_id: current_customer.id)
+     @shipping_cost = 800
     end
  private
     def order_params
