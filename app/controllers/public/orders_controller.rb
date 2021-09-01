30ba99553d9b02ec6befa736523284705b3b97b9
Keisukemuramatsu <harukasoranoyouni@gmail.com>
2021-09-01 04:45:05 +0000

    [Add]orderモデル保存

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -12,27 +12,29 @@ class Public::OrdersController < ApplicationController
         
     end
     def create
-        
+        @order = Order.new(order_params)
+        @order.customer_id = current_customer.id
+        @order.save
+        redirect_to complete_public_orders_path
     end
     def confirm
-    @order = Order.new(order_params)
-    if params[:order][:select_address] == "0"
-    @order.postal_code = current_customer.postal_code
-    @order.address = current_customer.address
-    @order.name = current_customer.last_name + current_customer.first_name
-    elsif params[:order][:select_address] == "1"
-    @address = Address.find(params[:order][:address_id])
-    @order.postal_code = @address.postal_code
-    @order.address = @address.address
-    @order.name = @address.name
-    elsif params[:order][:select_address] == "2"
-    @order.save
-    end
-     @cart_items = CartItem.where(customer_id: current_customer.id)
-     @shipping_cost = 800
+      @order = Order.new
+      if params[:order][:select_address] == "0"
+        @order.postal_code = current_customer.postal_code
+        @order.address = current_customer.address
+        @order.name = current_customer.last_name + current_customer.first_name
+      elsif params[:order][:select_address] == "1"
+        @address = Address.find(params[:order][:address_id])
+        @order.postal_code = @address.postal_code
+        @order.address = @address.address
+        @order.name = @address.name
+      elsif params[:order][:select_address] == "2"
+      end
+      @cart_items = CartItem.where(customer_id: current_customer.id)
+      @shipping_cost = 800
     end
  private
     def order_params
-        params.require(:order).permit(:payment_method, :postal_code, :address, :name)
+        params.require(:order).permit(:payment_method, :postal_code, :address, :name, :shipping_cost, :total_payment, :customer_id)
     end
 end
